9e865d9c231d60050f5ef6b4d1d986a4052bcdbc
haru <haru.vernal@gmail.com>
2026-09-25 23:10:44 +0900

    refactor(frontend): remove the unused enableCondensedLine preference

--- packages/frontend/src/preferences/def.ts
+++ packages/frontend/src/preferences/def.ts
@@ -352,9 +352,6 @@ export const PREF_DEF = definePreferences({
 	notificationStackAxis: {
 		default: 'horizontal' as 'vertical' | 'horizontal',
 	},
-	enableCondensedLine: {
-		default: true,
-	},
 	keepScreenOn: {
 		default: false,
 	},
